49b0cc9995660f37220e8bf2cf508be4b6645c51
karadsik7 <35919873+karadsik7@users.noreply.github.com>
2018-06-05 18:26:28 +0900

    DI - 자동생성

---
+++ ch02_DI_M_autocreation/src/classes/DClass.java
@@ -0,0 +1,16 @@
+package classes;
+
+public class DClass {
+
+	 AClass aClass;
+	 CClass cClass;
+
+	public void setaClass(AClass aClass) {
+		this.aClass = aClass;
+	}
+
+	public void setcClass(CClass cClass) {
+		this.cClass = cClass;
+	}
+	
+}
---
+++ ch02_DI_M_autocreation/src/classes/EClass.java
@@ -0,0 +1,17 @@
+package classes;
+
+public class EClass {
+	
+	 AClass aClass;
+	 DClass dClass;
+	
+	public void setaClass(AClass aClass) {
+		this.aClass = aClass;
+	}
+	public void setdClass(DClass dClass) {
+		this.dClass = dClass;
+	}
+	
+	
+	
+}
