3ea3330a7bba2145509f2ca05b11357dfc515d99
karadsik7 <35919873+karadsik7@users.noreply.github.com>
2018-06-05 18:33:49 +0900

    테스트 - @Before @After

---
+++ ch03_Test_E_before_after/src/com/inc/vo/UserVo.java
@@ -0,0 +1,39 @@
+package com.inc.vo;
+
+public class UserVo {
+	private String id, name, password;
+	
+	public UserVo() {}
+
+	public UserVo(String id, String name, String password) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
