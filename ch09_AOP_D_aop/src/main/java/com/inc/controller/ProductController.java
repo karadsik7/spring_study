9c0f4f7271be46d00acf8e231b4a407a88ad29c8
karadsik7 <35919873+karadsik7@users.noreply.github.com>
2018-06-15 17:33:42 +0900

    AOP - aop 태그를 사용한 트랜잭션 처리

---
+++ ch09_AOP_D_aop/src/main/java/com/inc/controller/ProductController.java
@@ -0,0 +1,68 @@
+package com.inc.controller;
+
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.inc.exception.ProductException;
+import com.inc.service.ProductService;
+import com.inc.service.ProductServiceImpl;
+import com.inc.vo.ProductVo;
+
+@Controller
+public class ProductController {
+
+	@Autowired
+	private ProductService productService;
+	
+	@RequestMapping(value="/product/list", method=RequestMethod.GET)
+	public String productList(Model model){
+		model.addAttribute("productMap", productService.productMap());
+		model.addAttribute("productInVo", new ProductVo());
+		model.addAttribute("productOutVo", new ProductVo());
+		return "/product/list.jsp";
+	}
+	
+	@RequestMapping(value="/product/add/in", method=RequestMethod.POST)
+	public String productAddIn(@ModelAttribute("productInVo") @Valid ProductVo productVo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("productOutVo", new ProductVo());
+			model.addAttribute("productMap", productService.productMap());
+			return "/product/list.jsp";
+		}
+		productService.addIn(productVo);
+		return "redirect:/product/list";
+	}
+	
+	@RequestMapping(value="/product/add/out", method=RequestMethod.POST)
+	public String productAddOut(@ModelAttribute("productOutVo") @Valid ProductVo productVo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			model.addAttribute("productInVo", new ProductVo());
+			model.addAttribute("productMap", productService.productMap());
+			return "/product/list.jsp";
+		}
+		try {
+			productService.addOut(productVo);
+		} catch (ProductException e) {
+			model.addAttribute("msg", e.getMessage());
+			model.addAttribute("url", "/product/list");
+			return "/error.jsp";
+		}catch(RuntimeException e) {
+			model.addAttribute("msg", "서버 오류입니다. 잠시 후 다시 시도하세요.");
+			model.addAttribute("url", "/product/list");
+			return "/error.jsp";
+		}
+		return "redirect:/product/list";
+	}
+	
+
+	
+	
+}
---
+++ ch09_AOP_D_aop/src/main/java/com/inc/exception/ProductException.java
@@ -0,0 +1,13 @@
+package com.inc.exception;
+
+public class ProductException extends RuntimeException{
+
+	public ProductException(String msg) {
+		super(msg);
+	}
+	
+	public ProductException() {
+		
+	}
+	
+}
