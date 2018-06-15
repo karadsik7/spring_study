94219a8bc9c6924d47fc692acb41f2fc62974618
karadsik7 <35919873+karadsik7@users.noreply.github.com>
2018-06-15 17:16:26 +0900

    MVC - Default Servlet Handler

---
+++ ch05_MVC_J_default_servlet_handler/src/com/inc/vo/UserVo.java
@@ -0,0 +1,56 @@
+package com.inc.vo;
+
+public class UserVo {
+	private String id, name, password, email;
+	
+	public UserVo(String id, String name, String password, String email) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+		this.email = email;
+	}
+	
+	public UserVo() {
+		
+	}
+
+	public UserVo(String id, String name, String password) {
+		this.id = id;
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	
+}
